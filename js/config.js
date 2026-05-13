65c1b27a215ee2d5e61942723d0713b0f14ccdd5
Joel Timoteo Gonza <tucorreo@gmail.com>
2026-05-12 23:08:26 -0500

    Configurar URL del Web App

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 ﻿const CONFIG = {
-  API_URL: 'PEGAR_AQUI_LA_URL_TERMINA_EN_exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxWIgvJzcVIzBA_9tUQqcjCLrcBoENDV9l2c2vD5FLLAAaw6OaVUUZJZu3kRwm2N0yo/exec',
   VERSION: '0.1.0',
   APP_NAME: 'Planta Rapel 2026'
 };
